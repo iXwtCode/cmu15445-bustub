771555daa631f09612b7ffa796cc531e265364dd
iXwtCode <114167160+iXwtCode@users.noreply.github.com>
2023-03-02 20:12:05 +0800

    all test ok

--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -61,8 +61,18 @@ auto Trie::Put(std::string_view key, T value) const -> Trie {
       auto ptr_new_root = std::make_shared<TrieNodeWithValue<T>>(ptr_value);
       return Trie(ptr_new_root);
     }
-    new_root = std::make_shared<TrieNode>();
-    ++m;
+
+    if(root_) {
+      if(root_->is_value_node_ || !root_->children_.empty()) {
+        new_root = root_->Clone();
+        ++m;
+      }
+    }
+    else{
+      new_root = std::make_shared<TrieNode>();
+      ++m;
+    }
+
   }
   else {
     new_root = std::shared_ptr<const TrieNode>(root_->Clone());
@@ -83,7 +93,7 @@ auto Trie::Put(std::string_view key, T value) const -> Trie {
     std::shared_ptr<TrieNode> ptr_new_node;
     if(ptr_cur_node->children_.count(key[i])) {
       auto node = ptr_cur_node->children_.at(key[i]);
-      if(node->is_value_node_) {
+      if(!node->children_.empty() || node->is_value_node_) {
         auto ptr = node->Clone();
         ptr_new_node = std::move(ptr);
       }
@@ -119,12 +129,69 @@ auto Trie::Put(std::string_view key, T value) const -> Trie {
 }
 
 auto Trie::Remove(std::string_view key) const -> Trie {
-  throw NotImplementedException("Trie::Remove is not implemented.");
+  //throw NotImplementedException("Trie::Remove is not implemented.");
 
   // You should walk through the trie and remove nodes if necessary. If the node doesn't contain a value any more,
   // you should convert it to `TrieNode`. If a node doesn't have children any more, you should remove it.
+
+  if(root_ == nullptr) { return Trie(nullptr); }
+  auto new_root = std::shared_ptr<const TrieNode>(root_->Clone());
+  auto vec_node = Walk(key, new_root);
+
+  int n = key.size(), m = vec_node.size();
+
+  //key存在
+  if(n == m) {
+    auto node = vec_node[n - 1]->children_.at(key[n - 1]);
+    int l = n; //删除后原key保留的长度，因为路径可能存在TrinodeWithValue节点，如国key所指向node不为叶节点则保留所有的可以的所有路径
+    if(node->children_.empty()) {
+      for(int i = n - 1; i >=0; --i) {
+        auto ptr_cur_node = vec_node[i];
+        if(ptr_cur_node->is_value_node_ || ptr_cur_node->children_.size() > 1) {
+          break;
+        }
+        --l;
+      }
+      if(l == 0) {
+        if(root_->children_.empty())
+          return Trie(nullptr);
+        new_root = std::shared_ptr(root_->Clone());
+        auto p = std::const_pointer_cast<TrieNode>(new_root);
+        p->children_.erase(key[0]);
+        return Trie(new_root);
+      }
+      auto ptr_cur_node = new_root;
+      for(int i = 1; i < l; ++i) {
+        auto ptr_new_node = std::shared_ptr<const TrieNode>(vec_node[i]->Clone());
+        auto ptr = std::const_pointer_cast<TrieNode>(ptr_cur_node);
+
+        ptr->children_[key[i - 1]] = ptr_new_node;
+        ptr_cur_node = ptr_new_node;
+      }
+      auto ptr = std::const_pointer_cast<TrieNode>(ptr_cur_node);
+      ptr->children_.erase(key[l - 1]);
+    }
+    else {
+      auto ptr_cur_node = new_root;
+      for(int i = 1; i < n; ++i) {
+        auto ptr_new_node = std::shared_ptr<const TrieNode>(vec_node[i]->Clone());
+        auto ptr = std::const_pointer_cast<TrieNode>(ptr_cur_node);
+
+        ptr->children_[key[i - 1]] = ptr_new_node;
+        ptr_cur_node = ptr_new_node;
+      }
+
+      auto ptr_tail_node = std::shared_ptr<const TrieNode>(ptr_cur_node->children_.at(key[n - 1])->Clone());
+      auto ptr_new_node = std::make_shared<TrieNode>(ptr_tail_node->children_);
+      //auto ptr_temp = dynamic_cast<const TrieNode *>(ptr_tail_node.get());
+      auto ptr = std::const_pointer_cast<TrieNode>(ptr_cur_node);
+      ptr->children_[key[n - 1]] = ptr_new_node;
+    }
+  }
+
+  return Trie(new_root);
 }
-auto Trie::Walk(std::string_view key, std::shared_ptr<const TrieNode> ptr_cur_node) const
+  auto Trie::Walk(std::string_view key, std::shared_ptr<const TrieNode> ptr_cur_node) const
     -> std::vector<std::shared_ptr<const TrieNode>> {
   std::vector<decltype(ptr_cur_node)> res;
 
