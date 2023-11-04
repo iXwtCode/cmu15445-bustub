a5215545bb50ff6b1ee8560629edc7f9a2eb1e33
iXwtCode <114167160+iXwtCode@users.noreply.github.com>
2023-11-04 15:25:44 +0800

    test

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -24,14 +24,20 @@ InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *
       table_info_(exec_ctx_->GetCatalog()->GetTable(plan_->table_oid_)) {}
 
 void InsertExecutor::Init() {
-  Tuple tup;
-  RID rid;
-  auto table_indexes = exec_ctx_->GetCatalog()->GetTableIndexes(table_info_->name_);
-
   num_inserted_ = 0;
   flag_ = false;
   child_executor_->Init();
-  while (child_executor_->Next(&tup, &rid)) {
+  //  std::cout << "end" << std::endl;
+}
+
+auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+  if (flag_) {
+    return false;
+  }
+  Tuple tup;
+  RID id;
+  auto table_indexes = exec_ctx_->GetCatalog()->GetTableIndexes(table_info_->name_);
+  while (child_executor_->Next(&tup, &id)) {
     TupleMeta meta{INVALID_TXN_ID, INVALID_TXN_ID, false};
     auto rid_insert = table_info_->table_->InsertTuple(meta, tup);
 
@@ -45,13 +51,6 @@ void InsertExecutor::Init() {
             rid_insert.value(), exec_ctx_->GetTransaction());
       }
     }
-  }
-  //  std::cout << "end" << std::endl;
-}
-
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
-  if (flag_) {
-    return false;
   }
   std::vector<Value> vec;
   vec.emplace_back(TypeId::INTEGER, num_inserted_);
